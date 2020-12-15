30f2496ee20772fa663ea8a64b7d3fda0a8a63c7
erezshi <erez.shi@gmail.com>
2020-12-15 19:40:56 +0200

    Update prod.tf

--- terraform/prod.tf
+++ terraform/prod.tf
@@ -152,7 +152,7 @@ resource "aws_elb" "prod_web" {
   security_groups = [aws_security_group.prod_web.id]
 
   listener {
-    instance_port     = var.default_port
+    instance_port     = var.jenkins_port
     instance_protocol = "http"
     lb_port           = 80
     lb_protocol       = "http"
